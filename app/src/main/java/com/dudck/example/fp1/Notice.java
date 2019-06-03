2950317eec99967c6cc65a28149f47efbacf0f32
dudckszmffjq <dudckszmffjq@naver.com>
2019-06-03 17:52:02 +0900

    Initial commit

---
+++ app/src/main/java/com/dudck/example/fp1/Notice.java
@@ -0,0 +1,26 @@
+package com.dudck.example.fp1;
+
+public class Notice {
+
+    String notice;
+    String name;
+    String date;
+
+    public Notice(String notice, String name, String date) {
+            this.notice = notice;
+            this.name = name;
+            this.date = date;
+    }
+
+    public String getNotice() {
+        return notice;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public String getDate() {
+        return date;
+    }
+}
